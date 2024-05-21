cc7b3abdc6e9a4693455891488ac5fc4387fd326
tundeafod <134690235+tundeafod@users.noreply.github.com>
2024-05-21 19:39:08 +0100

    committed

--- root/backend.tf
+++ root/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "tfstate-tspadp"
     key            = "tspadp-server/tfstate"
     dynamodb_table = "tspadp-backend"
-    region         = "eu-west-3"
+    region         = "eu-west-2"
     encrypt        = true
     profile        = "team1"
   }
