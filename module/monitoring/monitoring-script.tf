c15693f70648fb16941a58aa52229c3102efc3a1
tundeafod <134690235+tundeafod@users.noreply.github.com>
2024-05-23 13:43:26 +0100

    now working

--- module/monitoring/monitoring-script.tf
+++ module/monitoring/monitoring-script.tf
@@ -98,7 +98,7 @@ groups:
         annotations:
           summary: "Endpoint {{ $labels.instance }} down"
           description: "{{ $labels.instance }} of job {{ $labels.job }} has been down for more than 1 minutes."
-        
+
       - alert: HostOutOfMemory
         expr: node_memory_MemAvailable / node_memory_MemTotal * 100 < 25
         for: 2m
@@ -107,9 +107,9 @@ groups:
         annotations:
           summary: "Host out of memory (instance {{ $labels.instance }})"
           description: "Node memory is filling up (< 25% left)\n  VALUE = {{ $value }}\n  LABELS: {{ $labels }}"
-
+      
       - alert: HostOutOfDiskSpace
-        expr: (node_filesystem_avail{mountpoint="/"}  * 100) / node_filesystem_size{mountpoint="/"} < 50
+        expr: (node_filesystem_avail{mountpoint="/"} * 100) / node_filesystem_size{mountpoint="/"} < 50
         for: 1s
         labels:
           severity: warning
@@ -118,7 +118,7 @@ groups:
           description: "Disk is almost full (< 50% left)\n  VALUE = {{ $value }}\n  LABELS: {{ $labels }}"
 
       - alert: HostHighCpuLoad
-        expr: (sum by (instance) (irate(node_cpu{job="node_exporter_metrics",mode="idle"}[5m]))) > 80
+        expr: 100 - (avg by (instance) (irate(node_cpu_seconds_total{mode="idle"}[5m])) * 100) > 80
         for: 2m
         labels:
           severity: warning
@@ -232,7 +232,7 @@ global:
   smtp_smarthost: 'smtp.gmail.com:587'
   smtp_from: 'tunde.afod@gmail.com'
   smtp_auth_username: 'tunde.afod@gmail.com'
-  smtp_auth_password: 'Babatunde17'
+  smtp_auth_password: '{{ env "SMTP_AUTH_PASSWORD" }}'
   smtp_require_tls: true
 
 templates:
